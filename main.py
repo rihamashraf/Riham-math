9211ecf6ec2c4472b01dca43eda612465346a1f7
Riham Rayyan <144645290+rihamashraf@users.noreply.github.com>
2023-09-18 08:01:50 +0300

    hw

--- main.py
+++ main.py
@@ -8,12 +8,36 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def add(n1, n2):
+    # Use a breakpoint in the code line below to debug your script.
+    return n1 + n2  # Press ⌘F8 to toggle the breakpoint.
+
+def sub(n1, n2):
+    # Use a breakpoint in the code line below to debug your script.
+    return n1 - n2  # Press ⌘F8 to toggle the breakpoint.
+
+
+def div(n1, n2):
+    # Use a breakpoint in the code line below to debug your script.
+    return n1 / n2  # Press ⌘F8 to toggle the breakpoint.
 
 
 
+def mul(n1, n2):
+    # Use a breakpoint in the code line below to debug your script.
+    return n1 * n2  # Press ⌘F8 to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('hamza')
+    sum = add(3,8)
+    sub = sub(sum,3)
+    div = div(sub,2)
+    mul = mul(sum,3)
+
+    print_hi(sum)
+    print_hi(sub)
+    print_hi(div)
+    print_hi(mul)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
